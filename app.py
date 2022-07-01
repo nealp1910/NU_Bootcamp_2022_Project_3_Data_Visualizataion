4117998c20a0355e75706099287db3b99ce91937
Neel Patel <nealp1910@gmail.com>
2022-06-30 21:48:48 -0500

    update

--- app.py
+++ app.py
@@ -36,31 +36,6 @@ def welcome():
     return render_template('index.html')
 
 # this is an api route sending data from the database
-@app.route("/api/v1.0/data")
-def data1():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all movie titles"""
-    # Query all passengers
-    results = session.query(Movies.title, Movies.month, Movies.year, Movies.domestic_sales, Movies.international_sales, Movies.world_sales, Movies.movie_runtime).all()
-    session.close()
-    all_names = []
-    for title, month, year, domestic_sales, international_sales, world_sales, movie_runtime in results:
-        movie_dict = {}
-        movie_dict["title"] = title
-        movie_dict["month"] = month
-        movie_dict["year"] = year
-        movie_dict["domestic_sales"] = domestic_sales
-        movie_dict["international_sales"] = international_sales
-        movie_dict["world_sales"] = world_sales
-        movie_dict["movie_runtime"] = movie_runtime        
-        all_names.append(movie_dict)
-    # Convert list of tuples into normal list
-
-    return jsonify(all_names)
-
-# this is an api route sending data from the database
 @app.route("/api/v2.0/data")
 def data2():
     # Create our session (link) from Python to the DB
